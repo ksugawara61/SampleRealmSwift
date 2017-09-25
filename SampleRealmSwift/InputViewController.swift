98f22d0e19d481686f541c7b0f6d763dee7d2633
ksugawara61 <katsuya@kokohadoko.net>
2017-09-25 16:53:42 +0900

    Realmにデータほ保存する処理を実装

--- SampleRealmSwift/InputViewController.swift
+++ SampleRealmSwift/InputViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
 
@@ -24,7 +25,23 @@ class InputViewController: UIViewController {
     
     //Buttonが押された時の処理
     @IBAction func addButtonTapped(sender: AnyObject) {
+        let newTodo = Todo()
         
+        // textFieldに入力したデータをnewTodoのtitleに代入
+        newTodo.title = titleTextField.text!
+        
+        // 上記で代入したテキストデータを永続化するための処理
+        do{
+            let realm = try Realm()
+            try realm.write({ () -> Void in
+                realm.add(newTodo)
+                print("ToDo Saved")
+            })
+        }catch{
+            print("Save is Faild")
+        }
+        // 最後に、TableViewControllerに表示を切り替える
+        self.navigationController?.popToRootViewController(animated: true)
     }
 
 }
---
+++ SampleRealmSwift/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  SampleRealmSwift
+//
+//  Created by 菅原勝也 on 2017/09/25.
+//  Copyright © 2017年 instil. All rights reserved.
+//
+
+import RealmSwift
+
+class Todo: Object {
+    @objc dynamic var title = ""
+}
