de761173709222267d19ba45b5b8c987020c094c
ksugawara61 <katsuya@kokohadoko.net>
2017-09-25 17:02:06 +0900

    Realmのデータを取得する処理を実装

--- SampleRealmSwift/TableViewController.swift
+++ SampleRealmSwift/TableViewController.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TableViewController: UITableViewController {
+    
+    var todoItem: Results<Todo>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 永続化されているデータを取りだす
+        do{
+            let realm = try Realm()
+            todoItem = realm.objects(Todo.self)
+            tableView.reloadData()
+        }catch{
+            
+        }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     // 1. TableViewに表示するデータの個数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return todoItem.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        // todoItemに代入されたデータをobject:NSArrayに代入
+        let object = todoItem[indexPath.row]
+        
+        //cellのtextLabelのtextにobjectのtitleプロパティを代入
+        cell.textLabel?.text = object.title
         
         return cell
     }
